f117f8600019beee23f75735819d2626552d6bb4
vellmoss <vellmoss@gmail.com>
2015-05-20 10:15:27 +0300

    Add user following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if signed_in?
       @micropost  = current_user.microposts.build
-      @feed_items = current_user.feed.paginate(page: params[:page], per_page:8)
+      @feed_items = current_user.feed.paginate(page: params[:page], per_page:5)
     end
   end
 
